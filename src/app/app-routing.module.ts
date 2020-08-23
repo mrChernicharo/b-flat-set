a7473fb0f30a6d963931fcfc1389fd7f2a0d0a86
Felipe Chernicharo <string7dev@gmail.com>
2020-08-23 16:55:05 -0300

    auth link no sidenave - iniciando roteamento

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { AuthFormComponent } from './components/auth-form/auth-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // {path: '', component: AppComponent},
+  // {path: '/auth', component: AuthFormComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
