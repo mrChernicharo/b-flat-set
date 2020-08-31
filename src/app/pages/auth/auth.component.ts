6adacbb1b557e767598e8a3947b5f1f92355f91a
Felipe Chernicharo <string7dev@gmail.com>
2020-08-31 17:22:46 -0300

    auth forms ok, iniciando autenticação de usuários

--- src/app/pages/auth/auth.component.ts
+++ src/app/pages/auth/auth.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 /**
  * @title Basic expansion panel
  */
@@ -8,11 +9,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./auth.component.scss']
 })
 export class AuthComponent implements OnInit {
+  loginForm: FormGroup;
+  signupForm: FormGroup;
   panelOpenState = false;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.loginForm = new FormGroup({
+      'email': new FormControl(null, [Validators.required, Validators.email]),
+      'password': new FormControl(null, Validators.required)
+    })
+    this.signupForm = new FormGroup({
+      'username': new FormControl(null, Validators.required),
+      'email': new FormControl(null, [Validators.required, Validators.email]),
+      'password': new FormControl(null, Validators.required)
+    })
+  }
+
+
+  handleLogin() {
+    console.log(this.loginForm.value)
+  }
+
+  handleSignUp() {
+    console.log(this.signupForm.value)
+
   }
 
 }
